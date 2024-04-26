8610a3d51131a000d20aadf0b07716bf94dd3207
svyat5678 <svyat5678@gmail.com>
2024-04-26 10:37:04 +0300

    Реализован обход в глубину

---
+++ dfs.js
@@ -0,0 +1,38 @@
+function dfs(graph, startNode) {
+    let visited = [];
+    let pathLength = 0;
+
+    
+    function Util(node, count) {
+    
+        visited.push(node);
+    
+        if (node === endNode) {
+            pathLength = count;
+        }
+        
+        let neighbors = graph[node];
+        for (let i = 0; i < neighbors.length; i++) {
+            if (!visited.includes(neighbors[i])) {
+                Util(neighbors[i], count + 1);
+            }
+        }
+    }
+    
+    Util(startNode,0);
+    return pathLength;
+}
+    
+    
+    // Пример графа
+    let graph = {
+    A: ['B', 'C'],
+    B: ['A', 'D', 'E'],
+    C: ['A', 'F'],
+    D: ['B'],
+    E: ['B', 'F'],
+    F: ['C', 'E']
+    }; //комментари
+
+    let pathLength = dfs(graph, 'A', 'F');
+    console.log("Length of path from A to F: " + pathLength);
